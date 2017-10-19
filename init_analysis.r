3306ee9f424f17b28bbbef518ddc4c3dfe0399ce
FireflyII <fireflyii@users.noreply.github.com>
2017-10-19 16:29:44 -0700

    To see

--- init_analysis.r
+++ init_analysis.r
@@ -1,24 +1,71 @@
-setwd("/Users/jyoshimi/ipython/students/holman/holman_game_data")
+setwd("/Volumes/TarDisk-128/Game_Data/Game_Data/")
 
-cond_to_completion <- read.csv("data_for_r.csv")
+#cond_to_completion <- read.csv("data_for_r.csv")
+cond_to_completion <- read.csv("morestuff.csv")
+#Summary Statistics
 summary(cond_to_completion)
 boxplot(cond_to_completion$Completion_Time)
 
+# Left of ~ is Dependent Variable, Right is Independent Variable(s)
+
+#Analysis of Variance
+#Dependent is Completion Time .= With respect to every independent variable
 fit <-
-  aov(cond_to_completion$Completion_Time ~ .,
+  aov(Completion_Time ~ .,
       data = cond_to_completion)
 summary(fit)
 
-fit <- lm(cond_to_completion$Completion_Time ~ .,
+#Linear Model / Multiple Regression (multiple independent, one dependent)
+fit <- lm(Completion_Time ~ .,
           data = cond_to_completion)
 summary(fit)
 
+#Linear Model of Clear Completion
 fit_CC <-
   lm(cond_to_completion$Completion_Time ~ cond_to_completion$CC,
      data = cond_to_completion)
 summary(fit_CC)
-t.test(cond_to_completion$Completion_Time ~ cond_to_completion$CC)
-plot(cond_to_completion$CC, cond_to_completion$Completion_Time)
+
+################################
+# Clear Completion vs. Amount of time
+plot(cond_to_completion$Clear.Completion, cond_to_completion$Time.Spent)
+# Clear Completion vs. Number of buttons pushed
+plot(cond_to_completion$Clear.Completion, cond_to_completion$Buttons.Pushed)
+
+t.test(Buttons.Pushed ~ Clear.Completion, data = cond_to_completion)
+t.test(Buttons.Pushed ~ Clear.Progress, data = cond_to_completion)
+t.test(Buttons.Pushed ~ Clear.Rules, data = cond_to_completion)
+
+fit <-
+  aov(Buttons.Pushed ~ .,
+      data = cond_to_completion)
+summary(fit)
+
+fit <-
+  aov(Clear.Completion ~ .,
+      data = cond_to_completion)
+summary(fit)
+
+fit <-
+  aov(Clear.Progress ~ .,
+      data = cond_to_completion)
+summary(fit)
+
+fit <-
+  aov(Clear.Rules ~ .,
+      data = cond_to_completion)
+summary(fit)
+################################
+
+# T test of Clear Completion with respect to Number of Steps
+t.test(Completion_Time ~ CC,data = cond_to_completion)
+#summary(fit)
+
+ct <- cond_to_completion$Completion_Time
+ct
+t.test(Completion_Time ~ ct, data = cond_to_completion)
+
+plot(cond_to_completion$CC, cond_to_completion$Completion_Time,ylab="Steps to Completion",xlab="Unclear Vs. Clear Completion")
 
 fit_CP <-
   lm(cond_to_completion$Completion_Time ~ cond_to_completion$CP,
